ea040a36f04bd5de5434c90293138664bde38025
VthanhHai <101922208+VthanhHai@users.noreply.github.com>
2022-06-19 17:04:42 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,49 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp1
+namespace xepgach
 {
     internal class chuongtrinh
     {
         static void Main(string[] args)
         {
-            /* 
-            Console.WriteLine("Hello :))");
-            int a = 1;
-            Console.WriteLine(a);
-            Console.WriteLine('a');//kí tự
-            Console.WriteLine("abc");//chuỗi
-            Console.WriteLine(true);
-            Console.WriteLine(false);
-            const string hi = "hello";
-            Console.WriteLine(hi);
-            // hi = "Xin chào"; // có const thì ko dc khai báo thêm.
-            Console.WriteLine(hi);
-            */
-            // Lệnh nhập xuất.
-            Console.WriteLine(" bạn tên gì ? ;");
-            string name = Console.ReadLine();
-            
-           
-            int yob = Convert.ToInt32(Console.ReadLine());
-            int age = 2022 - yob;
-            string noio= Console.ReadLine();
-           Console.WriteLine("hello " + name + ", bạn được" + age +", nơi ở:"+ noio);
-            
-        }
-        /*
-        hàm dùng để so sanh 2 số nguyên.
-        input: 2 số nguyên
-        output:
-        nếu a<b thì trả 1
-        nếu a>b thì trả về 2
-        ngược lại trả về 0
-        // hải
-        */
-        int Compare(int a,int b)
-        {
-            return 1;
+            int x = 0, y = 0;
+            int x_old, y_old;
+            Console.SetCursorPosition(x, y);// gán tọa độ cho game.
+            Console.ForegroundColor = ConsoleColor.Red;//thay đổi màu chữ
+            Console.WriteLine("10");
+            Console.BackgroundColor = ConsoleColor.Black;//thay đổi phông nền
+            do
+            {
+                x_old = x;y_old = y;
+                ConsoleKeyInfo banphim;// khai báo nút di chuyển
+                banphim = Console.ReadKey();
+                if (banphim.Key == ConsoleKey.Escape)// gán nút Exit game.
+                    break;
+                switch (banphim.Key)
+                {
+                    //gán nút di chuyển.
+                    case ConsoleKey.LeftArrow:x--; break;
+                        
+                    case ConsoleKey.RightArrow:x++; break;
+                        
+                    case ConsoleKey.UpArrow:y--; break;
+                        
+                    case ConsoleKey.DownArrow:y++; break;
+                        
+                }
+
+                //Console.Clear();
+                Console.ForegroundColor= ConsoleColor.Black;//chùng với màu nền.
+                Console.SetCursorPosition(x_old, y_old); // old tọa độ cũ 
+                Console.WriteLine("10");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(x,y);
+                Console.WriteLine("10");
+
+
+            } while (true);
+
         }
     }
 }
-
